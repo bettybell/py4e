38d4c72d2cb6a133a6ca385c59e23fc4d8788de8
bettybell <54127847+bettybell@users.noreply.github.com>
2019-10-15 11:40:00 +0800

    Add files via upload

---
+++ Assignment 10.py
@@ -0,0 +1,17 @@
+count = 0
+while True:
+    fname = input("Enter the file name: ")
+    if fname == "NA NA BOO BOO" :
+        print("NA NA BOO BOO - You have been punk'd!")
+        break
+    try:
+        fhand = open(fname)
+    except:
+        print('File cannot be opened:', fname)
+        continue
+    break
+for line in fhand:
+    if line.startswith("Subject:"):
+        count = count + 1
+print("There were", count, "subject lines in", fname)
+exit()
